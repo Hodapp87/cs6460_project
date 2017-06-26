625876f495ddd5e8b4a5068691826b7fe52b1ef7
Chris Hodapp <hodapp87@gmail.com>
2017-06-26 13:06:32 -0400

    Added some commented-out bower stuff in shell.nix

--- shell.nix
+++ shell.nix
@@ -17,6 +17,23 @@ let
     name = "emacsLeanbook";
     buildInputs = [emacs-custom pkgs.python3 pkgs.mercurial];
     # Also needs: TeX
+
+    # Add "myWebApp ? { outPath = ./.; name = "myWebApp"; }" to
+    # arguments of this file to use this (and still one must run 'eval
+    # $buildPhase' in the shell first if using this in nix-shell)
+    #
+    # To produce bower-packages.nix, make a bower.json with:
+    # {"name": "my-web-app", "dependencies": {"juicy-ace-editor": "~2.0.1"}}
+    # ...and run this through bower2nix.
+    #
+    #bowerComponents = pkgs.buildBowerComponents {
+    #  name = "my-web-app";
+    #  generated = ./bower-packages.nix;
+    #  src = myWebApp;
+    #};
+    #buildPhase = ''
+    #cp  --reflink=auto --no-preserve=mode -R $bowerComponents/bower_components .
+    #'';
   }
 
 # python3 -m http.server
