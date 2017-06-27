e1b5dab21202aa0d53a03971f9a109d818e36856
Chris Hodapp <hodapp87@gmail.com>
2017-06-26 20:33:44 -0400

    Working fragment example

--- js/main_live.js
+++ js/main_live.js
@@ -796,7 +796,24 @@ myModule2.append_console("-- Loading pocket.gl...            ");
 params = {
     copyright: "CS6460 Draft",
     editorTheme: "dark",
-    fluidWidth: true
+    fluidWidth: true,
+    fragmentShaderFile: "./basic_fragment.glsl",
+    uniforms: [
+        { type: "float", value: 1.0, min: 0.0, max: 4.0, name: "z_speed", GUIName: "Z Speed" },
+        { type: "float", value: 1.0, min: 0.0, max: 4.0, name: "rot_speed", GUIName: "Rotation Speed" },
+        { type: "float", value: 1.0, min: 0.0, max: 1.0, name: "twist", GUIName: "Twist" },
+        { type: "integer", value: 90, min: 60, max: 120, name: "fov", GUIName: "FOV" },
+        { type: "float", value: 0, min: -0.7, max: 0.7, name: "hOffset", GUIName: "Horiz Offset" },
+        { type: "float", value: 0, min: -0.7, max: 0.7, name: "vOffset", GUIName: "Vertical Offset" },
+    ],
+    textures: [
+        { 
+            url: "grid.jpg", 
+            wrap: "repeat", // repeat (default), clamp
+            name: "texture"
+        }
+    ],
+    animated: true,
 }
 
 window.addEventListener("load", function () { 
